a336a2e0bc83e11d81c837c8428b391afe65fc17
CarlosSantiago Nevett <csnevett@gmail.com>
2021-01-14 17:51:26 -0500

    working on obj

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,10 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
 
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+const cards = document.querySelector(".cards");
+
+axios
+  .get("https://api.github.com/users/elCarlosSantiago")
+  .then((res) => {
+    const gitData = res.data;
+    const newGitCard = gitCardMaker({
+      avatar_url: gitData.avatar_url,
+      name: gitData.name,
+      login: gitData.login,
+      location: gitData.location,
+      html_url: gitData.html_url,
+      followers: gitData.followers,
+      following: gitData.following,
+      bio: gitData.bio,
+    });
+    cards.appendChild(newGitCard);
+  })
+  .catch((err) => {
+    debugger;
+  });
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -31,6 +52,59 @@ import axios from 'axios';
 */
 
 const followersArray = [];
+
+function gitCardMaker({
+  avatar_url,
+  name,
+  login,
+  location,
+  html_url,
+  followers,
+  following,
+  bio,
+}) {
+  // Instantiate the elements
+  const gitCard = document.createElement("div");
+  const avatar = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const gitName = document.createElement("h3");
+  const username = document.createElement("p");
+  const locationP = document.createElement("p");
+  const profile = document.createElement("p");
+  const gitLink = document.createElement("a");
+  const gitFollowers = document.createElement("p");
+  const gitFollowing = document.createElement("p");
+  const gitBio = document.createElement("p");
+  // Set class names, attributes & text
+
+  gitCard.classList.add("card");
+  avatar.src = avatar_url;
+  cardInfo.classList.add("card-info");
+  gitName.classList.add("name");
+  gitName.textContent = name;
+  username.classList.add("username");
+  username.textContent = login;
+  locationP.textContent = location;
+  profile.textContent = "Profile";
+  gitLink.href = html_url;
+  gitLink.textContent = html_url;
+  gitFollowers.textContent = followers;
+  gitFollowing.textContent = following;
+  gitBio.textContent = bio;
+  // creating hierarchy
+  gitCard.appendChild(avatar);
+  gitCard.appendChild(cardInfo);
+  cardInfo.appendChild(gitName);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(locationP);
+  cardInfo.appendChild(profile);
+  profile.appendChild(gitLink);
+  cardInfo.appendChild(gitFollowers);
+  cardInfo.appendChild(gitFollowing);
+  cardInfo.appendChild(bio);
+  console.log(gitCard);
+  return gitCard;
+}
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
